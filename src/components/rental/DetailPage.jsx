6bda387f056384d36a6897f490d4bf7a1eda3cab
Maharta <gedemaharta.dev@gmail.com>
2023-02-21 20:16:41 +0800

    added detail page.

---
+++ src/components/rental/DetailPage.jsx
@@ -0,0 +1,64 @@
+import Card from "react-bootstrap/Card";
+import Carousel from "react-bootstrap/Carousel";
+import { CarouselItem } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { detailCard, detail } from "./DetailPage.module.css";
+import { db } from "../../../firebase";
+
+function DetailPage() {
+  const { id } = useParams();
+  const [rentalData, setRentalData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function getDetailById() {
+      const docRef = doc(db, "rental", id);
+      try {
+        const docSnap = await getDoc(docRef);
+        setRentalData(docSnap.data());
+        console.log(docSnap.data());
+      } catch (error) {
+        // eslint-disable-next-line no-alert
+        alert(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getDetailById();
+  }, [id]);
+
+  if (isLoading) {
+    return <div>Loading..</div>;
+  }
+
+  return (
+    <div className={detailCard}>
+      <h1>{rentalData?.name}</h1>
+      <Carousel fade>
+        {rentalData?.images.map((image) => (
+          <CarouselItem key={image}>
+            <div style={{ margin: "10px", height: "260px" }}>
+              <Card.Img
+                variant="top"
+                src={image}
+                style={{ objectFit: "cover" }}
+              />
+            </div>
+          </CarouselItem>
+        ))}
+      </Carousel>
+      <section className={detail}>
+        <address>Lokasi: {rentalData.location}</address>
+        <address>{rentalData.address}</address>
+        <p>Rata-rata harga: {rentalData.price}</p>
+        <p>Fasilitas: {rentalData.facility}</p>
+        <p>Kelengkapan: {rentalData.amenities}</p>
+        <p>No telepon: {rentalData.phone}</p>
+      </section>
+    </div>
+  );
+}
+
+export default DetailPage;
